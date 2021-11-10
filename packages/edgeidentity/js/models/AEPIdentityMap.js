30ce6f7bfa611d65059d40754a49582afaa0a58d
Calise Cheung <cacheung@adobe.com>
2021-11-09 16:02:12 -0800

    Add Edge Identity API in JS and Java (#74)
    * Add EdgeIdentity API in js
    
    * add import statements for RCTAEPEdgeIdentityModule.java
    
    * Update with review comments
    
    * testing branch
    
    * Update getIdentities
    
    Update getIdentities
    
    * remove add items
    
    * update EdgeIdentity.js
    
    * More Update to the IdentityMap
    
    * more updates
    
    * Update with android databridge  and removeIdentity
    
    * cleaning code
    
    * Remove console codes
    
    Remove console codes
    
    * Remove public access for internal files
    
    Remove public access for internal files
    
    * update with review comments
    
    update with review comments
    
    * update review comments
    
    update review comments
    
    * Add the boolean check for Primary key
    
    Add the boolean check for Primary key
    
    * update typo for ts file
    
    update typo for ts file
    
    * Update AuthenticatedState.js
    
    Update AuthenticatedState.js
    
    * Update with review comments
    
    Update with review comments

---
+++ packages/edgeidentity/js/models/AEPIdentityMap.js
@@ -0,0 +1,52 @@
+/*
+Copyright 2021 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+
+@flow
+@format
+*/
+
+'use strict';
+
+import AEPIdentityItem from './AEPIdentityItem';
+
+class AEPIdentityMap {
+  items: {string: Array<AEPIdentityItem>} = {};
+  namespace:string;
+
+  constructor() {}
+
+  /**
+   * @brief Add Items to Identity Item
+  */
+  addItem(item: AEPIdentityItem, namespace: string) {
+    if (item === null) {
+      console.log("add - ignore addItem, item can't be null");
+      return;
+  }
+
+    if (namespace === null) {
+     console.log("add - ignore addItem, namespaces can't be null");
+     return;
+  }
+
+     // add item to the existing namespace
+  if (this.items[namespace] !== undefined) {
+      var list = this.items[namespaces];
+      list.push(item);
+      this.items[namespace] = list;     
+  } else {
+      // creates new list with the item in it
+      this.items[namespace] = [item] 
+    }
+  }
+}
+
+module.exports = AEPIdentityMap;
