435f3f2b78515c217f2099116a4bbc9f9677ac70
Yansong <yanyang@adobe.com>
2021-11-30 16:48:01 -0600

    Rename module : AEPAssurance -> Assurance

--- packages/assurance/js/index.js
+++ packages/assurance/js/index.js
@@ -17,7 +17,7 @@ governing permissions and limitations under the License.
 
 module.exports = {
   // Native modules
-  get AEPAssurance() {
+  get Assurance() {
     return require('./Assurance');
   },
 };
