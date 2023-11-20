cb60519fb6c5b6132323cf449c8675ab03f7db75
dsoffiantini <dsoffiantini@gmail.com>
2023-11-20 12:57:50 -0700

    feat: :sparkles: adds support for code based to messaging

--- apps/AEPSampleApp/android/app/src/main/java/com/aepsampleapp/MainApplication.java
+++ apps/AEPSampleApp/android/app/src/main/java/com/aepsampleapp/MainApplication.java
@@ -10,130 +10,94 @@ REPRESENTATIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 */
 
-package com.aepsampleapp;
+package com.reactnative.aepsampleapp;
 
 import android.app.Application;
-import android.content.Context;
 import com.adobe.marketing.mobile.Assurance;
 import com.adobe.marketing.mobile.Edge;
 import com.adobe.marketing.mobile.Extension;
+import com.adobe.marketing.mobile.Identity;
 import com.adobe.marketing.mobile.Lifecycle;
 import com.adobe.marketing.mobile.LoggingMode;
 import com.adobe.marketing.mobile.Messaging;
 import com.adobe.marketing.mobile.MobileCore;
-import com.adobe.marketing.mobile.Signal;
-import com.adobe.marketing.mobile.UserProfile;
-import com.adobe.marketing.mobile.edge.consent.Consent;
-import com.adobe.marketing.mobile.edge.bridge.EdgeBridge;
-import com.adobe.marketing.mobile.optimize.Optimize;
 import com.adobe.marketing.mobile.Places;
+import com.adobe.marketing.mobile.Signal;
 import com.adobe.marketing.mobile.Target;
-import com.aepsampleapp.newarchitecture.MainApplicationReactNativeHost;
+import com.adobe.marketing.mobile.UserProfile;
+import com.adobe.marketing.mobile.edge.bridge.EdgeBridge;
+import com.adobe.marketing.mobile.edge.consent.Consent;
+import com.adobe.marketing.mobile.optimize.Optimize;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.soloader.SoLoader;
-import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+  private final ReactNativeHost mReactNativeHost =
+      new DefaultReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for
-      // example: packages.add(new MyReactNativePackage());
-      return packages;
-    }
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here,
+          // for example: packages.add(new MyReactNativePackage());
+          return packages;
+        }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
 
-  private final ReactNativeHost mNewArchitectureNativeHost =
-      new MainApplicationReactNativeHost(this);
+        @Override
+        protected boolean isNewArchEnabled() {
+          return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+        }
+
+        @Override
+        protected Boolean isHermesEnabled() {
+          return BuildConfig.IS_HERMES_ENABLED;
+        }
+      };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      return mNewArchitectureNativeHost;
-    } else {
-      return mReactNativeHost;
-    }
+    return mReactNativeHost;
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
-    // If you opted-in for the New Architecture, we enable the TurboModule
-    // system
-    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry
+      // point for this app.
+      DefaultNewArchitectureEntryPoint.load();
+    }
+    ReactNativeFlipper.initializeFlipper(
+        this, getReactNativeHost().getReactInstanceManager());
     MobileCore.setApplication(this);
     MobileCore.setLogLevel(LoggingMode.VERBOSE);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-  }
-
-  /**
-   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
-   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-   *
-   * @param context
-   * @param reactInstanceManager
-   */
-  private static void
-  initializeFlipper(Context context,
-      ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-        We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.aepsampleapp.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class,
-                ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-      MobileCore.configureWithAppID("YOUR-APP-ID");
-      List<Class<? extends Extension>> extensions = Arrays.asList(
-          Lifecycle.EXTENSION,
-          Signal.EXTENSION,
-          Edge.EXTENSION,
-          com.adobe.marketing.mobile.edge.identity.Identity.EXTENSION,
-          EdgeBridge.EXTENSION,
-          Messaging.EXTENSION,
-          UserProfile.EXTENSION,
-          Assurance.EXTENSION,
-          Places.EXTENSION,
-          Target.EXTENSION,
-          Optimize.EXTENSION,
-          Consent.EXTENSION);
-      MobileCore.registerExtensions(extensions, o -> {
-        MobileCore.lifecycleStart(null);
-      });
-    }
+    MobileCore.configureWithAppID("YOUR-APP-ID");
+    List<Class<? extends Extension>> extensions = Arrays.asList(
+        Lifecycle.EXTENSION, Signal.EXTENSION, Edge.EXTENSION,
+        com.adobe.marketing.mobile.edge.identity.Identity.EXTENSION,
+        EdgeBridge.EXTENSION, Messaging.EXTENSION, UserProfile.EXTENSION,
+        Assurance.EXTENSION, Places.EXTENSION, Target.EXTENSION,
+        Optimize.EXTENSION, Consent.EXTENSION, com.adobe.marketing.mobile.Identity.EXTENSION);
+    MobileCore.registerExtensions(extensions,
+                                  o -> { MobileCore.lifecycleStart(null); });
   }
 }
