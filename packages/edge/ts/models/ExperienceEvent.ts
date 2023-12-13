97debeada7be8c95fbf785401a68b5449c3dfb86
Calise Cheung <cacheung@adobe.com>
2023-12-12 18:54:15 -0800

    Update ExperienceEvent
    Update ExperienceEvent

--- packages/edge/ts/models/ExperienceEvent.ts
+++ packages/edge/ts/models/ExperienceEvent.ts
@@ -1,58 +1,42 @@
 type ExperienceEventArguments =
-    {
-      xdmData?: Record<string, any>;
+  | {
+      xdmData: Record<string, any>;
       data?: Record<string, any> | null;
-      datasetID?: string | null;
+      datasetIdentifier?: string | null;
     }
   | {
-      xdmData?: Record<string, any>;
+      xdmData: Record<string, any>;
       data?: Record<string, any> | null;
-      datastreamIdOverrides: string | null;
-      datastreamConfigOverride: Record<string, any> | null;
+      datastreamIdOverride?: string | null;
+      datastreamConfigOverride?: Record<string, any> | null;
     };
 
 class ExperienceEvent {
-  xdmData?: Record<string, any>;
+  xdmData: Record<string, any>;
   data?: Record<string, any>;
-  datasetID?: string;
-  datastreamIdOverrides?: string;
+  datasetIdentifier?: string;
+  datastreamIdOverride?: string;
   datastreamConfigOverride?: Record<string, any>;
 
   constructor(args: ExperienceEventArguments);
+  constructor(xdmData: Record<string, any>, data?: Record<string, any> | null, datasetIdentifier?: string | null);
   constructor(
-    xdmData?: Record<string, any>,
+    argsOrXdmData: ExperienceEventArguments | Record<string, any>,
     data?: Record<string, any> | null,
-    datasetID?: string | null,
-  );
-  constructor(
-    argsOrXdmData?: ExperienceEventArguments | Record<string, any>,
-    data?: Record<string, any> | null,
-    datasetID?: string | null,
+    datasetIdentifier?: string | null,
   ) {
-    if (argsOrXdmData && typeof argsOrXdmData === 'object') {
-      // Handle case where a single argument object
+    if (typeof argsOrXdmData === 'object' && 'xdmData' in argsOrXdmData) {
       const args = argsOrXdmData as ExperienceEventArguments;
-      if ('datasetID' in args) {
-        // Object contains xdmData, data, and datasetID
-        const { xdmData, data, datasetID } = args;
-        this.xdmData = xdmData;
-        this.data = data;
-        this.datasetID = datasetID;
-      } else if ('dataStreamIdOverrides' in args && 'datastreamConfigOverride' in args) {
-        // Object contains xdmData, data, datastreamIdOverrides, and datastreamConfigOverride
-        const { xdmData, data, datastreamIdOverrides, datastreamConfigOverride } = args;
-        this.xdmData = xdmData;
-        this.data = data;
-        this.datastreamIdOverrides = datastreamIdOverrides;
-        this.datastreamConfigOverride = datastreamConfigOverride;
-      }
+      this.xdmData = args.xdmData;
+      this.data = args.data;
+      this.datasetIdentifier = 'datasetIdentifier' in args ? args.datasetIdentifier : undefined;
+      this.datastreamIdOverride = 'datastreamIdOverride' in args ? args.datastreamIdOverride : undefined;
+      this.datastreamConfigOverride = 'datastreamConfigOverride' in args ? args.datastreamConfigOverride : undefined;
     } else {
-      // Handle case where separate parameters
-      this.xdmData = argsOrXdmData;
+      this.xdmData = argsOrXdmData as Record<string, any>;
       this.data = data;
-      this.datasetID = datasetID;
+      this.datasetIdentifier = datasetIdentifier;
     }
   }
 }
-
 export default ExperienceEvent;
