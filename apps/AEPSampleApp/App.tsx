2d7d7e9fe84811ce72dadfb981af38ecbab093fb
Calise Cheung <cacheung@adobe.com>
2024-04-08 14:31:26 -0700

    bring messaging and optimize back
    bring messaging and optimize back

--- apps/AEPSampleApp/App.tsx
+++ apps/AEPSampleApp/App.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import {Button, View} from 'react-native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
+import OptimizeView from './extensions/OptimizeView';
 import ProfileView from './extensions/ProfileView';
+import MessagingView from './extensions/MessagingView';
 import CoreView from './extensions/CoreView';
 import IdentityView from './extensions/IdentityView';
 import ConsentView from './extensions/ConsentView';
@@ -29,6 +31,16 @@ function HomeScreen({navigation}: NavigationProps) {
       <Button
         onPress={() => navigation.navigate('IdentityView')}
         title="Identity"
+      />
+
+      <Button
+        onPress={() => navigation.navigate('MessagingView')}
+        title="Messaging"
+      />
+
+      <Button
+        onPress={() => navigation.navigate('OptimizeView')}
+        title="Optimize"
       />
 
       <Button onPress={() => navigation.navigate('EdgeView')} title="Edge" />
@@ -79,6 +91,8 @@ export default function App() {
         <Drawer.Screen name="EdgeView" component={EdgeView} />
         <Drawer.Screen name="EdgeIdentityView" component={EdgeIdentityView} />
         <Drawer.Screen name="IdentityView" component={IdentityView} />
+        <Drawer.Screen name="MessagingView" component={MessagingView} />
+        <Drawer.Screen name="OptimizeView" component={OptimizeView} />
         <Drawer.Screen name="PlacesView" component={PlacesView} />
         <Drawer.Screen name="ProfileView" component={ProfileView} />
         <Drawer.Screen name="TargetView" component={TargetView} />
--- apps/AEPSampleApp/ios/AEPSampleApp/AppDelegate.h
+++ apps/AEPSampleApp/ios/AEPSampleApp/AppDelegate.h
@@ -19,11 +19,13 @@ governing permissions and limitations under the License.
 @import AEPLifecycle;
 @import AEPIdentity;
 @import AEPUserProfile;
+@import AEPMessaging;
 @import AEPEdge;
 @import AEPEdgeIdentity;
 @import AEPEdgeConsent;
 @import AEPEdgeBridge;
 @import AEPAssurance;
+@import AEPOptimize;
 @import AEPPlaces;
 @import AEPTarget;
 @import AEPCampaignClassic;
