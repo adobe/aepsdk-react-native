97813eb636ec8c7d9b8ff16a72c915301682baf5
yansong <yanyang@adobe.com>
2024-04-22 17:11:09 -0500

    update wrapper versions to 6.0.1

--- apps/AEPSampleApp/android/app/src/main/java/com/aepsampleapp/MainActivity.kt
+++ apps/AEPSampleApp/android/app/src/main/java/com/aepsampleapp/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "aepsampleapp"
+  override fun getMainComponentName(): String = "AEPSampleApp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- apps/AEPSampleApp/ios/AEPSampleApp/AppDelegate.mm
+++ apps/AEPSampleApp/ios/AEPSampleApp/AppDelegate.mm
@@ -17,7 +17,7 @@ governing permissions and limitations under the License.
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"aepsampleapp";
+  self.moduleName = @"AEPSampleApp";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
