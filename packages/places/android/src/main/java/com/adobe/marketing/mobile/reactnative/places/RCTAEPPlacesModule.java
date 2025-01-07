a4663f0de62fa487127557c92fdb6f8f916784ab
namArora3112 <namarora@adobe.com>
2025-01-07 10:46:38 +0530

    places native module name fixes in android (#412)

--- packages/places/android/src/main/java/com/adobe/marketing/mobile/reactnative/places/RCTAEPPlacesModule.java
+++ packages/places/android/src/main/java/com/adobe/marketing/mobile/reactnative/places/RCTAEPPlacesModule.java
@@ -43,7 +43,7 @@ public class RCTAEPPlacesModule extends ReactContextBaseJavaModule {
 
   @Override
   public String getName() {
-    return "RCTAEPPlaces";
+    return "AEPPlaces";
   }
 
   @ReactMethod
