1ce3bc276fd7475fb9f197524cf4fffb875f0473
namarora <namarora@adobe.com>
2025-02-21 10:36:37 +0530

    mobile core initialise api added

--- packages/core/android/src/main/java/com/adobe/marketing/mobile/reactnative/RCTAEPCoreModule.java
+++ packages/core/android/src/main/java/com/adobe/marketing/mobile/reactnative/RCTAEPCoreModule.java
@@ -71,6 +71,58 @@ public class RCTAEPCoreModule extends ReactContextBaseJavaModule {
         MobileCore.configureWithAppID(appId);
     }
 
+@ReactMethod
+public void initializeWithAppId(String appId, final Promise promise) {
+//    Log.debug(LOG_TAG, "Initializing with appId: " + appId);
+    Log.debug(getName(), "Initializing with appId" ,appId);
+    MobileCore.initialize((Application) reactContext.getApplicationContext(), appId, o -> promise.resolve(null));
+    }
+
+    @ReactMethod
+    public void initialize(ReadableMap initOptionsMap, final Callback callback) {
+        InitOptions initOptions = null;
+
+        if (initOptionsMap.hasKey("appId")) {
+
+            String appId = initOptionsMap.getString("appId");
+            Log.debug(getName(), "Initializing with appId" ,appId);
+
+            initOptions = InitOptions.configureWithAppID(appId);
+        } 
+
+        if (initOptions == null) {
+            callback.invoke("InitOptions must contain either an appId or a filePath", null);
+            return;
+        }
+
+        if (initOptionsMap.hasKey("lifecycleAutomaticTrackingEnabled")) {
+            initOptions.setLifecycleAutomaticTrackingEnabled(initOptionsMap.getBoolean("lifecycleAutomaticTrackingEnabled"));
+        }
+
+        if (initOptionsMap.hasKey("lifecycleAdditionalContextData")) {
+            ReadableMap contextDataMap = initOptionsMap.getMap("lifecycleAdditionalContextData");
+            if (contextDataMap != null) {
+                ReadableMapKeySetIterator iterator = contextDataMap.keySetIterator();
+                while (iterator.hasNextKey()) {
+                    String key = iterator.nextKey();
+                    initOptions.getLifecycleAdditionalContextData().put(key, contextDataMap.getString(key));
+                }
+            }
+        }
+
+        MobileCore.initialize((Application) reactContext.getApplicationContext(), initOptions, new AdobeCallbackWithError<Void>() {
+            @Override
+            public void call(Void value) {
+                callback.invoke(null, null);
+            }
+            // should we override this fail method or not?
+            @Override
+            public void fail(AdobeError adobeError) {
+                callback.invoke(adobeError.getErrorName(), null);
+            }
+        });
+    }
+
     @ReactMethod
     public void initialize(ReadableMap initOptionsMap, final Promise promise) {
         InitOptions initOptions = initOptionsFromMap(initOptionsMap);
