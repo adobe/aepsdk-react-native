ec9cfd1b68dad202119d56d398d7ece8e1e98ff2
Ishita Gambhir <igambhir@adobe.com>
2025-07-24 11:36:43 +0530

    fix UTs

--- packages/messaging/__tests__/MessagingTests.ts
+++ packages/messaging/__tests__/MessagingTests.ts
@@ -10,7 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { NativeEventEmitter, NativeModules } from 'react-native';
+import { NativeModules } from 'react-native';
 import { Messaging, Message, MessagingEdgeEventType } from '../src';
 
 describe('Messaging', () => {
