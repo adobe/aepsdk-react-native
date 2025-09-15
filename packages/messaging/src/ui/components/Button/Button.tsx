cdcbdc162e70d9c0f8ac9ebd29bc6e3a6a7db177
Calise Cheung <cacheung@adobe.com>
2025-09-15 11:49:00 -0700

    Fix failing test
    fix failing test

--- packages/messaging/src/ui/components/Button/Button.tsx
+++ packages/messaging/src/ui/components/Button/Button.tsx
@@ -9,7 +9,7 @@
     ANY KIND, either express or implied. See the License for the specific
     language governing permissions and limitations under the License.
 */
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 import {
   GestureResponderEvent,
   Linking,
