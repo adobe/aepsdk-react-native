1db16a1a18c1e874cf935a6fed75f0dbb303ce86
Calise Cheung <cacheung@adobe.com>
2025-09-28 10:13:36 -0700

    remove conversion
    remove conversion

--- packages/messaging/src/Messaging.ts
+++ packages/messaging/src/Messaging.ts
@@ -257,7 +257,6 @@ class Messaging {
       return [];
     }
     const contentCards = propositions
-      .map((proposition) => new MessagingProposition(proposition))
       .flatMap((proposition) =>
         proposition.items.filter(
           (item) => item.schema === PersonalizationSchema.CONTENT_CARD
