d0ef51cd2803945d1125cbff0df712c45db7a404
Naman Arora <namarora+adobe@adobe.com>
2025-12-09 21:01:16 +0530

    RN0.80 and RN0.81 with old arc

---
+++ apps/RNApp081/App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+import { NewAppScreen } from '@react-native/new-app-screen';
+import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { Messaging } from '@adobe/react-native-aepmessaging';
+
+const messagingExtensionVersion = async () => {
+  const version = await Messaging.extensionVersion();
+  console.log(`AdobeExperienceSDK: Messaging version: ${version}`);
+};
+
+function App() {
+  React.useEffect(() => {
+    messagingExtensionVersion();
+  }, []);
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <NewAppScreen templateFileName="App.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ apps/RNApp081/metro.config.js
@@ -0,0 +1,43 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ */
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { resolve, join } = require('path');
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  watchFolders: [resolve(__dirname, '../../packages')],
+  resolver: {
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (target, name) => {
+          if (typeof name !== 'string') {
+            return target[name];
+          }
+          if (
+            name &&
+            name.startsWith &&
+            name.startsWith('@adobe/react-native-aep')
+          ) {
+            const packageName = name.replace('@adobe/react-native-aep', '');
+            console.log('------packageName -> ' + packageName);
+            return join(__dirname, `../../packages/${packageName}`);
+          }
+          return join(__dirname, `node_modules/${name}`);
+        },
+      },
+    ),
+  },
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
