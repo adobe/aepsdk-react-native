15d72ee4b4846f4d3029f887a83ff3fc5d981cfc
Ishita Gambhir <igambhir@adobe.com>
2025-12-18 15:54:28 +0530

    fix dependencies for UTs

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   maxConcurrency: 10,
-  preset: './apps/AEPSampleAppNewArchEnabled/node_modules/react-native/jest-preset.js',
+  preset: 'react-native',
   transform: {
-    '^.+\\.(js)$': '<rootDir>/node_modules/babel-jest',
-    '\\.(ts|tsx)$': 'ts-jest'
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
+  ],
   setupFiles: ['./tests/jest/setup.ts'],
   testMatch: ['**/packages/**/__tests__/*.ts'],
   modulePaths: ['node_modules', './apps/AEPSampleAppNewArchEnabled/node_modules'],
   testPathIgnorePatterns: ['./packages/template'],
   moduleDirectories: ['node_modules', './apps/AEPSampleAppNewArchEnabled/node_modules'],
-  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
