fc99583b6bb4b280eab9b7dfdee856299df7481b
Ishita Gambhir <igambhir@adobe.com>
2026-02-05 15:15:11 +0530

    upgrade RN version to 0.81

--- apps/AEPSampleApp/ios/AEPSampleApp/AppDelegate.mm
+++ apps/AEPSampleApp/ios/AEPSampleApp/AppDelegate.mm
@@ -12,26 +12,27 @@ governing permissions and limitations under the License.
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <ReactAppDependencyProvider/RCTAppDependencyProvider.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   self.moduleName = @"AEPSampleApp";
+  self.dependencyProvider = [RCTAppDependencyProvider new];
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-  return [self getBundleURL];
+  return [self bundleURL];
 }
 
-- (NSURL *)getBundleURL
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
