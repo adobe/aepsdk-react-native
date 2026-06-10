d4d038f20cced6801fb9ecb45442d44f38faf3bd
namArora3112 <namarora@adobe.com>
2026-06-10 09:24:27 +0530

    removed agents docs along with circle ci fixes

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const { defaults: tsjPreset } = require("ts-jest/presets");
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   ...tsjPreset,
-  preset: "@react-native/jest-preset",
+  preset: "react-native",
   testEnvironment: "node",
   transform: {
     "^.+\\.jsx$": "babel-jest",
@@ -17,10 +17,7 @@ module.exports = {
   setupFiles: ["<rootDir>/tests/jest/setup.ts"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   moduleNameMapper: {
-    // Resolve react-native from the E2E test app's local node_modules so root
-    // devDependencies never need to include react-native (avoids hoisting conflicts
-    // between apps that use different RN versions).
-    "^react-native$": "<rootDir>/apps/AwesomeProject/node_modules/react-native",
+    "^react-native$": "<rootDir>/node_modules/react-native",
   },
   testPathIgnorePatterns: [
     "/node_modules/",
